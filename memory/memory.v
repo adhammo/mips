2f54ddf627e21af0f70f628ba0bca243665da5bc
Toka <toka.eldorghamy1@gmail.com>
2022-02-28 21:41:05 +0200

    Add testbench for Memory

--- memory/memory.v
+++ memory/memory.v
@@ -1,18 +1,18 @@
-module memory #(parameter DATAWIDTH=16, parameter ADDRWIDTH=20) (
+module memory (
   input clk,
   input write, enable,
-  input [ADDRWIDTH-1:0] addr,
-  input [DATAWIDTH-1:0] in,
-  output wire [DATAWIDTH-1:0] out
+  input [19:0] addr,
+  input [15:0] in,
+  output wire [15:0] out
 );
 
-  reg [DATAWIDTH-1: 0] mem[2**ADDRWIDTH-1:0];
+  reg [15: 0] mem[(2**20)-1:0];
 
   // Memory
   always @(posedge clk) begin
-    if(!write) mem[addr] <= in;  // write input (active-low)
+    if(!write) 
+      mem[addr] <= in;  // write input (active-low)
   end
 
-  assign out = !enable ? mem[addr] : {DATAWIDTH{1'bz}}; // enable output (active-low)
-
+  assign out = (!enable)? mem[addr] : {(16){1'bz}}; // enable output (active-low)
 endmodule
